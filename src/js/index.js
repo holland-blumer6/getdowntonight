099ba3a800556f7cd4ab9262d257a56098b66547
holland-blumer6 <90846635+holland-blumer6@users.noreply.github.com>
2022-04-03 18:38:44 -0400

    tracking seconds

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
-require('./module1.js');
-//console.log('starting up!');
+import $ from 'jquery';
+
+var seconds = 0;
+
+
+function countTime(){
+    $('#main').html("You\'ve been here for "+ seconds + " seconds");
+    seconds += 1;
+}
+
+var run = setInterval(countTime,1000);
+
+
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
+                use: [ 
+                    {loader: 'babel-loader'} 
+                ]
             },
         ],
     },
