48ccc20fcf1d065c8ac9fe1672deb1cb3e7edfae
holland-blumer6 <90846635+holland-blumer6@users.noreply.github.com>
2022-04-03 18:46:07 -0400

    potential memory leak with seconds

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,10 @@
 import $ from 'jquery';
 
-var seconds = 0;
+let seconds = 0;
 
-
-function countTime(){
-    $('#main').html("You\'ve been here for "+ seconds + " seconds");
-    seconds += 1;
+function countTime() {
+  $('#main').html(`You've been here for ${seconds} seconds`);
+  seconds += 1;
 }
 
-var run = setInterval(countTime,1000);
-
-
-
-
-
+setInterval(countTime, 1000);
