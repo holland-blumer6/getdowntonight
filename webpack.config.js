d8f488dd06fc373d1e8335ccea610f64cdcaa72a
holland-blumer6 <90846635+holland-blumer6@users.noreply.github.com>
2022-04-03 18:44:26 -0400

    added eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,28 @@
 const env = process.env.NODE_ENV || 'development';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: env,
-    entry: ['./src/js'], // this is where our app lives
-    devtool: 'source-map', // this enables debugging with source in chrome devtools
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [ 
-                    {loader: 'babel-loader'} 
-                ]
-            },
+  mode: env,
+  entry: ['./src/js'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/html/index.html',
-            filename: './index.html',
-            favicon: './src/images/dartmouth_gradient_logo.png',
-        }),
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/html/index.html',
+      filename: './index.html',
+      favicon: './src/images/dartmouth_gradient_logo.png',
+    }),
+    new ESLintPlugin({}),
+  ],
 };
-
